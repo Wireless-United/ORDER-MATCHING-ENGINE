16b64049c983ad8af116024feade99bce7ff9890
pranaav2k5 <pranaavjeemain@gmail.com>
2025-08-26 01:46:21 +0530

    created orderbook template

---
+++ src/order-book/ask_book.rs
@@ -0,0 +1,20 @@
+// AskBook Template
+// ---------------
+// Represents the sell side of the order book (lowest price on top).
+//
+// Suggested structure:
+// - Use BTreeMap<Price, VecDeque<Order>> for price levels (ascending order).
+// - Methods: add_order, remove_order, get_best_ask, etc.
+//
+// Fill in with actual logic as needed.
+
+// use std::collections::{BTreeMap, VecDeque};
+// use crate::order_book::order::Order;
+
+// pub struct AskBook {
+//     // ...fields...
+// }
+//
+// impl AskBook {
+//     // ...methods...
+// }
---
+++ src/order-book/bid_book.rs
@@ -0,0 +1,20 @@
+// BidBook Template
+// ---------------
+// Represents the buy side of the order book (highest price on top).
+//
+// Suggested structure:
+// - Use BTreeMap<Price, VecDeque<Order>> for price levels (descending order).
+// - Methods: add_order, remove_order, get_best_bid, etc.
+//
+// Fill in with actual logic as needed.
+
+// use std::collections::{BTreeMap, VecDeque};
+// use crate::order_book::order::Order;
+
+// pub struct BidBook {
+//     // ...fields...
+// }
+//
+// impl BidBook {
+//     // ...methods...
+// }
